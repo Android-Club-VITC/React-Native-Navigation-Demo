880983e455dfb8388ab7fcecaa96d48814e5e3c8
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-08-29 02:05:19 +0530

    refactor

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import TabNav from "./TabNav";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -9,40 +8,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  header: {
-    fontSize: 40,
-    fontWeight: "bold",
-    color: "#335EA1",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    height: 40,
-  },
-  input: {
-    borderWidth: 2,
-    width: 300,
-    margin: 10,
-    borderStyle: "solid",
-    borderColor: "black",
-    fontSize: 20,
-    padding: 10,
-  },
-
-  optionsButton: {
-    flexDirection: "row",
-    gap: 10,
-    position: "absolute",
-    bottom: 60,
-  },
-  methodContainer: {
-    position: "absolute",
-    top: 70,
-  },
-});
